f0d8fd4b3fda71daae0c89b00ed8f02fb854cd67
Nigel Nkomo <mrnbnkomo@gmail.com>
2023-03-07 09:59:23 +0100

    add initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import fetchBio from "./api";
 
 function App() {
+  const [person, setPerson] = useState("Michael");
+  const [bio, setBio] = useState(null);
+
+  useEffect(() => {
+    fetchBio(person).then((result) => {
+      setBio(result);
+    });
+
+    //cleanup
+    //otherwise loading won't show and the page will show the same value until the new value is fetched because the value of bio won't be null anymore
+    // unless == if not
+    // if bio isn't true show loading
+    // bio stays null for 2s when the value is Jemma
+    return () => setBio(null);
+  }, [person]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <select value={person} onChange={(e) => setPerson(e.target.value)}>
+        <option value="Michael">Michael</option>
+        <option value="Jemma">Jemma</option>
+        <option value="Sylvester">Sylvester</option>
+      </select>
+      <br />
+      <hr />
+      <br />
+      <p>
+        <i>{bio ?? "Loading..."}</i>
+      </p>
+    </>
   );
 }
 
